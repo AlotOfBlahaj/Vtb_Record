bb29c0862474a500b3c218cf54f4ba9f9322a581
FZxiao233 <fzxiao@outlook.com>
2020-01-30 15:47:28 +0800

    添加更多meta信息
    Signed-off-by: FZxiao233 <fzxiao@outlook.com>

--- src/plugins/worker/uploader.go
+++ src/plugins/worker/uploader.go
@@ -8,26 +8,28 @@ import (
 )
 
 type UploadDict struct {
-	Title       string
-	Filename    string
-	Date        string
-	Path        string
-	User        string
-	OriginTitle string `json:"Origin_Title"`
-	ASS         string
-	Txt         string
+	Title        string
+	Filename     string
+	Date         string
+	Path         string
+	User         string
+	OriginTitle  string `json:"Origin_Title"`
+	ASS          string
+	Txt          string
+	OriginTarget string `json:"originTarget"`
 }
 
 func UploadVideo(video *structUtils.VideoInfo) {
 	u := UploadDict{
-		Title:       video.Title,
-		Filename:    video.FileName,
-		Date:        video.Date,
-		Path:        video.FilePath,
-		User:        video.UsersConfig.Name,
-		OriginTitle: video.Title,
-		ASS:         "",
-		Txt:         "",
+		Title:        video.Title,
+		Filename:     video.FileName,
+		Date:         video.Date,
+		Path:         video.FilePath,
+		User:         video.UsersConfig.Name,
+		OriginTitle:  video.Title,
+		ASS:          "",
+		Txt:          "",
+		OriginTarget: video.Target,
 	}
 	data, _ := json.Marshal(u)
 	log.Println(string(data))
